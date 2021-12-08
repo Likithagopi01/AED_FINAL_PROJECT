41f476a314fdc98d18341c83bfb8da85fdb94177
Sharmadha Parthiban <parthiban.s@northeastern.edu>
2021-12-07 22:35:00 -0500

    deliveryman

---
+++ db4odemo/src/Business/DeliveryMan/DeliveryMan.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.DeliveryMan;
+
+/**
+ *
+ * @author Ash
+ */
+import Business.Organization;
+import Business.Role.DeliverManRole;
+import Business.Role.PharamacyAdminRole;
+import Business.Role.Role;
+import java.util.ArrayList;
+
+public class DeliveryMan extends Organization {
+    
+    String deliveryManName;
+    
+
+    public String getDeliveryManName() {
+        return deliveryManName;
+    }
+
+    
+    public void setDeliveryManName(String deliveryManName) {
+        this.deliveryManName = deliveryManName;
+    }
+
+
+    
+    @Override
+    public String toString(){
+        return deliveryManName;
+    }
+
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new DeliverManRole());
+        return roles;
+    }
+    
+    
+}
